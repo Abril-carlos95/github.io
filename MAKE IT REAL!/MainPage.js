5b88f4905dcf1a315e3cbf534a9cd5720da069fb
Abril-carlos95 <59939704+Abril-carlos95@users.noreply.github.com>
2020-01-15 17:38:15 -0500

    Add files via upload

---
+++ MAKE IT REAL!/MainPage.js
@@ -0,0 +1,31 @@
+let count = (str, char) => {
+    let counter = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            counter += 1
+        }
+    }
+    console.log(counter)
+    return counter
+}
+
+console.log(count("hello", "l"));
+
+
+var numb = [3, 12, 45, 7];
+let filter = numb.filter(num => {
+    return num > 10;
+});
+console.log("Results for array filter: " + filter);
+
+
+const hypotenuse = (a, b) => {
+    let pow = x => x * x;
+    let sum = pow(a) + pow(b);
+    return Math.sqrt(sum);
+}
+
+console.log("hypotenuse for 10 and 13: " + hypotenuse(10, 13));
+
+
+
